bf8907984ef6ea898f31bc9582410a1da1b69e29
AmilSrinath <amilsrinath5@gmail.com>
2023-12-07 22:40:53 +0530

    Amil

---
+++ src/main/java/com/example/FormController.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FormController {
+    public TextField jTextFild1;
+    public Label jLabal2;
+    public ImageView imageView;
+    ImageIcon img;
+    public void btnOnAction(ActionEvent actionEvent) {
+        try{
+            String str = jTextFild1.getText().trim();
+            String path = System.getProperty("user.dir")+"/";
+            String chartset = "UTF-8";
+            Map<EncodeHintType, ErrorCorrectionLevel> hashMap = new HashMap<EncodeHintType,ErrorCorrectionLevel>();
+            hashMap.put(EncodeHintType.ERROR_CORRECTION,ErrorCorrectionLevel.L);
+            generateQRcode(str,path+str+".png",chartset,hashMap,50,50);
+            img = new ImageIcon(str+".png");
+            jLabal2.setText("");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void generateQRcode(String data, String path, String charset, Map map, int h, int w) throws WriterException, IOException {
+        BitMatrix matrix = new MultiFormatWriter().encode(new String(data.getBytes(charset), charset), BarcodeFormat.QR_CODE, w, h);
+        MatrixToImageWriter.writeToFile(matrix, path.substring(path.lastIndexOf('.') + 1), new File(path));
+    }
+}
---
+++ src/main/java/com/example/Start.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import com.google.zxing.NotFoundException;
+
+public class Start {
+    public static void main(String[] args) throws NotFoundException {
+        Main.main(args);
+    }
+}
